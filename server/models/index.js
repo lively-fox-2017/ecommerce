9ae933872b73681eaac8e9b687403a2e22dd4027
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-24 22:14:43 +0700

    create item model

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const User = require('./user');
+const Item = require('./item');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ecommerce');
 mongoose.Promise = global.Promise;
 
-module.exports = User;
+module.exports = { User, Item }
