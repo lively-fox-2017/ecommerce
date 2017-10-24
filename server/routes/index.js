ce711aa1478450a05932f924e019dd707cfda4f8
Dimitri <deathmitri@gmail.com>
2017-10-24 16:48:00 +0700

    Added Item CRUD API

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const customerRoutes = require('./customers');
+const itemRoutes = require('./items');
 
 router.use('/customers', customerRoutes);
+router.use('/items', itemRoutes);
 
 router.get('/', (req, res) => {
   res.status(200).json({
