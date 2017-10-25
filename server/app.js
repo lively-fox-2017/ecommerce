43d14abbc9d74b3336bcfb0589fae95fbe5aa7e6
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-25 20:02:17 +0700

    add 404 error handler

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const decodeToken = require('./helpers/decode-token');
+const error404 = require('./helpers/error-404');
+
 const admin = require('./routes/admin');
 const item = require('./routes/item');
 const user = require('./routes/user');
@@ -17,9 +20,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// after this middleware runs,
+// access user information in req.headers.user
+app.use(decodeToken);
+
 app.use('/admin', admin);
 app.use('/item', item);
 app.use('/user', user);
 
+// 404 handler
+app.use('/', error404);
 
 app.listen(3000, console.log('ecommerce server listening on port 3000'));
