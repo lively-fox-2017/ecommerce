f99bc551d66aa3baa57654974c000a3f44a02873
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-25 13:06:02 +0700

    setup routes on app.js

--- server/app.js
+++ server/app.js
@@ -6,12 +6,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const admin = require('./routes/admin');
+const item = require('./routes/item');
+const user = require('./routes/user');
+
 const app = express();
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', admin);
+app.use('/item', item);
+app.use('/user', user);
 
 
 app.listen(3000, console.log('ecommerce server listening on port 3000'));
