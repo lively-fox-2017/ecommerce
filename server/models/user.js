e99a9c7046714e4f132780ebf4b4f0648fac21f5
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-25 18:39:41 +0700

    create hook for hashing password before save and add compare method in user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
@@ -8,5 +9,27 @@ let UserSchema = new Schema({
 	password: String,
 	role: String
 });
+
+// hash the password before user is saved to the database
+UserSchema.pre('save', function(next) {
+	let user = this;
+
+	bcrypt.hash(user.password, 8, function(err, hash) {
+		if (err) return next(err);
+		user.password = hash;
+		next();
+	});
+});
+
+// validate password
+UserSchema.methods.comparePassword = function(password) {
+	return new Promise((resolve, reject) => {
+		bcrypt.compare(password, this.password, function(err, isMatch) {
+    	if (err) reject(err);
+    	else if (!isMatch) reject('wrong password');
+    	else resolve(isMatch)
+  	});
+	});
+};
 
 module.exports = mongoose.model('User', UserSchema);
