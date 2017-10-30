ecb8c6165bcd471597d902dd1303b3774900a05a
setiadijoe <setiadi_y56@yahoo.co.id>
2017-10-30 14:00:05 +0700

    back end sudah tinggal fitur add to cart belum

--- client/script.js
+++ client/script.js
@@ -2,9 +2,17 @@ new Vue({
     el:"#app",
     data:{
         message: 'Welcome Aboard!',
-        items: []
+        items: [],
+        cart: []
     },
-    mounted: function(){
+    methods:{
+        buy: function(product){
+            console.log(this.cart);
+            this.cart.push({"name": product.itemname, "price": product.price})
+            console.log(this.cart);
+        }
+    },
+    mounted: function () {
         var self = this
         axios.get('http://localhost:3000/items')
             .then(function (response) {
@@ -19,17 +27,22 @@ new Vue({
 })
 
 
-Vue.component('todo-item', {
-    props: ['todo'],
+Vue.component('product-item', {
+    props: ['product'],
     template: `<div class="four wide column">
-    <div class="ui card">
-                <img v-bind:src="todo.imageUrl" v-bind:alt="todo.itemname" width="300px" heigth="100px">
-                    <div class="ui vertical animated button" tabindex="0">
-                            <div class="hidden content" id="shopnow">$ {{todo.price}}</div>
+                <div class="ui card">
+                <img v-bind:src="product.imageUrl" v-bind:alt="product.itemname" width="300px" heigth="100px">
+                    <div class="ui vertical animated button" tabindex="0" @buy="buy(product)">
+                            <div class="hidden content">$ {{product.price}}</div>
                             <div class="visible content">
                                 <i class="shop icon"></i>
                             </div>
                         </div>
                     </div>
-                </div>`
+                </div>`,
+    methods: {
+        buy: function (product) {
+            this.$emit('buy', product)
+        }
+    }
 })
