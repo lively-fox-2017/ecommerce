aa637714b6db78944efb926105bb9c8620bb8234
kalibani <kalibani.ka@gmail.com>
2017-12-18 18:13:44 +0700

    update add to chart

--- client/script.js
+++ client/script.js
@@ -1,11 +1,38 @@
 new Vue({
   el:'#app',
   data: {
-    message: 'hello',
-    counter:0,
-    items:[]
-  }, methods:{
-    addCart: function (product) {
+    counter:'',
+    items:[],
+    search:''
+  },
+
+  computed: {
+    carts:{
+      get(){
+        let carts = localStorage.getItem("cart")
+        if (carts) {
+          return JSON.parse(carts);
+        }else {
+          console.log('carts kosong');
+        }
+      }
+    },
+
+    filteredProduct() {
+      var self = this
+      return this.items.filter(function(item){
+        return item.nama.toLowerCase().indexOf(self.search.toLowerCase())>=0;
+      });
+    }
+  },
+
+  beforeMount(){
+    this.getAll(),
+    this.qty()
+  },
+
+  methods:{
+    addCart(product) {
       if (this.carts) {
         let obj_cart = this.carts
         obj_cart.push(product)
@@ -14,31 +41,27 @@ new Vue({
         var cart2 = []
         cart2.push(product)
         localStorage.setItem('cart', JSON.stringify(cart2))
+        location.reload()
       }
       this.qty()
     },
 
     qty() {
-      this.counter = this.carts.length
-    }
-  },
-
-  created(){
-    axios.get('http://localhost:3001/items')
-    .then((response)=>{
-      this.items = response.data
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-  },
-
-  computed: {
-    carts:{
-      get() {
-        let carts = localStorage.getItem("cart")
-        return JSON.parse(carts);
+      if (!this.carts) {
+        this.counter = 0
+      }else {
+        this.counter = this.carts.length
       }
+    },
+
+    getAll(){
+      axios.get('http://localhost:3001/items')
+      .then((response)=>{
+        this.items = response.data
+      })
+      .catch(err=>{
+        console.log(err);
+      })
     }
   }
 
